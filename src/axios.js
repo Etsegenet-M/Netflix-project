8932f1909ea818710667a8588a108dced10c1b80
Etse <etselegesse@gmail.com>
2022-07-25 19:36:50 -0400

    first commit

---
+++ src/axios.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+// this is base url to make requests to the movie database
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
+// instance.get("/foo-bar");
+
+export default instance;
